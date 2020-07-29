a8b34f8155a7658c8cbd116224c74b549eb8a0a5
harika penukonda <penukondaharika@gmail.com>
2020-07-29 13:23:48 -0500

    Service Implementation

---
+++ src/com/udemy/springdemo/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.udemy.springdemo.service;
+
+import java.util.List;
+
+import com.udemy.springdemo.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+
+}
