0113ddfee28f902b67ad1e030bc6f247c457b889
harika penukonda <penukondaharika@gmail.com>
2020-07-29 10:22:48 -0500

    Create CustomerDAO.java

---
+++ src/com/udemy/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.udemy.springdemo.dao;
+
+import java.util.List;
+
+import com.udemy.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
